8c407d081e21bbc2c4a9b4975dd876f095294cc0
WU JIAMING <841056496@qq.com>
2023-07-24 18:47:14 +0800

    prac_03

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+#The smallest number I can see is 5 and the largest is 20.
+
+print(random.randrange(3, 10, 2))  # line 2
+#The smallest number I can see is 3 and the largest is 9.
+# This could not have produced a 4 since 4 is not in the sequence.
+
+print(random.uniform(2.5, 5.5))  # line 3
+#The smallest number I can see is 2.5 and the largest is 5.5.
+
+print(random.randint(1, 100))
